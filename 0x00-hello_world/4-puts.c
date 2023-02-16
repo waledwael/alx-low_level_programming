332b910aeaa3c51ce374ebadc8a0f8bfc4646f75
Waled Wael <waledwael0@gmail.com>
2023-02-16 11:36:12 -0800

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*
- * main - A program that prints a line with puts function
+/**
+ * main - A program that prints that prints a line using puts
  * Return : 0 (Success)
  */
 int main(void)
