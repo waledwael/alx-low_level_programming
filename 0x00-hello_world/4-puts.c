9196998473cfb23bae6c6b5568d173b75dc4b61b
Waled Wael <waledwael0@gmail.com>
2023-02-16 11:57:33 -0800

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that prints that prints a line using puts
- * Return : 0 (Success)
+ *
+ * Return: 0 (Success)
  */
-int main(void)
+int  main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A a C program that prints a line using printf
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
