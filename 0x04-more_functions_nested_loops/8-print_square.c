054ec9a06c65ff5a8a1e595ba4287a8d14bff89c
Waled Wael <waledwael0@gmail.com>
2023-02-23 06:35:40 -0800

    C

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,13 +7,16 @@
 void print_square(int size)
 {
 	int i, j;
-	
+
 	if (size <= 0)
 		_putchar('\n');
-	for (i = 1; i <= size; i++)
+	else
 	{
-		for (j = size; j > 0; j--)
-			_putchar('#');
-		_putchar('\n');
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
