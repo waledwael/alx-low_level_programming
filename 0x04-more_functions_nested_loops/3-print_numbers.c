fa4c8f9abd92a77a2be59cb033b4b4bc868bee3b
Waled Wael <waledwael0@gmail.com>
2023-02-23 04:20:39 -0800

    C

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers -  prints the numbers, from 0 to 9,
+ * followed by a new line
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		_putchar('0' + x);
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers -  prints the numbers, from 0 to 9
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x != 2 && x != 4)
+			_putchar('0' + x);
+		else
+			continue;
+	}
+}
