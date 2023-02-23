001f69e1b9cbbf21991a48d0a983285f06415cdb
Waled Wael <waledwael0@gmail.com>
2023-02-23 08:24:12 -0800

    C

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -11,4 +10,4 @@ void print_remaining_days(int month, int day, int year);
 
 int convert_day(int month, int day);
 
-#endif
+#endif /* MAIN_H */
