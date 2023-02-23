48b7dc40572d8c68c53b2bccfb671156868e985f
Waled Wael <waledwael0@gmail.com>
2023-02-23 07:09:59 -0800

    C

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - prints an integer.
+ * @n: integer
+ * Return : void
+ */
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = - n;
+	}
+	else
+		i = n;
+	if (i / 10)
+		print_number(i / 10);
+	_putchar('0' + (i % 10));
+}
+
