4a93a0d17c089f929164b3db9a340e8b90fb9e05
Waled Wael <waledwael0@gmail.com>
2023-02-28 06:28:37 -0800

    C

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer.
+ * @s: string input
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int c = 0;
+	int result = 0;
+	int sign = 1;
+	int is_number = 0;
+
+	while (s[c])
+	{
+		if (s[c] == 45)
+			sign *= -1;
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			is_number = 1;
+			result = result * 10 + (s[c] - '0');
+			c++;
+		}
+		if (is_number == 1)
+			break;
+		c++;
+	}
+	result *= sign;
+	return (result);
+}
