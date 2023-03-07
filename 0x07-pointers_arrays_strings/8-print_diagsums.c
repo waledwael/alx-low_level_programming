fd79b02baedf224ae57621d25ddabacdeed285b6
Waled Wael <waledwael0@gmail.com>
2023-03-06 16:55:52 -0800

    C

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,10 +11,10 @@ void print_diagsums(int *a, int size)
 {
 	int i, res1 = 0, res2 = 0;
 
-	for (i = 0; i <= (size * size); i = i + size - 1)
+	for (i = 0; i < (size * size); i = i + size + 1)
 		res1 += a[i];
 
-	for (i = size - 1; i < ((size * size) - size); i = i + size - 1)
+	for (i = size - 1; i <= ((size * size) - size); i = i + size - 1)
 		res2 += a[i];
 	printf("%d, %d\n", res1, res2);
 }
