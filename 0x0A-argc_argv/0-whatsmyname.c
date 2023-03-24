5732dcd31627a1b0badf1a277f90221a794f9e90
Waled Wael <waledwael0@gmail.com>
2023-03-24 05:51:20 -0700

    C

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array of arguments
  * Return: 0
  */
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
